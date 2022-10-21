6664f5c36e48a903b566907168c87d28cdfe9681
Cauê Braz <cauealvesbraz@gmail.com>
2022-10-21 20:59:12 -0300

    chore: add EditorConfig file

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "this" {
     name = var.name
-    
+
     encryption_configuration {
         encryption_type = var.encryption.type
         kms_key = var.encryption.kms_key
