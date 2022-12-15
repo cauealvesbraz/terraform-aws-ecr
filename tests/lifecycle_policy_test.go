9c503ebcd671bf856a7aa83843c0421e9ec59106
Cauê Braz <cauealvesbraz@gmail.com>
2022-12-15 19:11:29 -0300

    chore(tests): test policy with multiple rules

--- tests/lifecycle_policy_test.go
+++ tests/lifecycle_policy_test.go
@@ -86,3 +86,63 @@ func TestCanCreateRepositoryWithLifecyclePolicyAndTaggedStatus(t *testing.T) {
 	lifecyclePolicyJson, _ := json.Marshal(lifecyclePolicy)
 	assert.JSONEq(t, string(lifecyclePolicyJson), currentPolicy)
 }
+
+func TestCanCreateRepositoryWithLifecyclePolicyAndUntaggedStatusAndMultipleRules(t *testing.T) {
+	t.Parallel()
+
+	expectedName := fmt.Sprintf("test-%d", random.Random(0, 10))
+
+	region := aws.GetRandomRegion(t, nil, nil)
+
+	lifecyclePolicy := map[string]interface{}{
+		"rules": []map[string]interface{}{
+			{
+				"rulePriority": 1,
+				"description":  "Expire images older than 14 days",
+				"selection": map[string]interface{}{
+					"tagStatus":   "untagged",
+					"countType":   "sinceImagePushed",
+					"countUnit":   "days",
+					"countNumber": 14,
+				},
+				"action": map[string]interface{}{
+					"type": "expire",
+				},
+			},
+			{
+				"rulePriority": 2,
+				"description":  "Expire images older than 30 days",
+				"selection": map[string]interface{}{
+					"tagStatus":   "untagged",
+					"countType":   "sinceImagePushed",
+					"countUnit":   "days",
+					"countNumber": 30,
+				},
+				"action": map[string]interface{}{
+					"type": "expire",
+				},
+			},
+		},
+	}
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../",
+		Vars: map[string]interface{}{
+			"name":             expectedName,
+			"lifecycle_policy": lifecyclePolicy,
+		},
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": region,
+		},
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	repository := aws.GetECRRepo(t, region, expectedName)
+	currentPolicy := aws.GetECRRepoLifecyclePolicy(t, region, repository)
+
+	lifecyclePolicyJson, _ := json.Marshal(lifecyclePolicy)
+	assert.JSONEq(t, string(lifecyclePolicyJson), currentPolicy)
+}
